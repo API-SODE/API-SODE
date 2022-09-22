f73bc5e7fae9ca11b79350b2c1dae4f2fe3a9600
민선 <kor3334@naver.com>
2022-09-22 09:33:28 +0900

    [ADD] Springboot init
    스프링 부트 초기 설정 with intellij

---
+++ lied/lied/src/main/java/com/example/lied/LiedApplication.java
@@ -0,0 +1,13 @@
+package com.example.lied;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiedApplication.class, args);
+	}
+
+}
