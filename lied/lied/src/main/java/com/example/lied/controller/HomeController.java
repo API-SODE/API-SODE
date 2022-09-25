2412b8e920af23f297b2de0a13f9c06cfe019028
민선 <kor3334@naver.com>
2022-09-25 15:13:35 +0900

    [ADD]  home, list page
    -타임리프 설정 -정적파일 경로 설절

---
+++ lied/lied/src/main/java/com/example/lied/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.lied.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "html/home";
+    }
+
+    @RequestMapping("/list")
+    public String goToList() {
+        return "html/list";
+    }
+}
