24870b714a24a77c0248a2c5c11041178eaebe65
jiwon <83341978+jiione@users.noreply.github.com>
2022-11-10 23:35:44 +0900

    질문 데이터 수정

--- lied/lied/src/main/resources/static/js/list.js
+++ lied/lied/src/main/resources/static/js/list.js
@@ -2,3 +2,15 @@ var data_list = [];
 
 data_list.push("나는 친구에게 실망한 적이 있다.");
 data_list.push("나는 지금 좋아하는 사람이 있다.");
+data_list.push("나는 지금 집에 가고 싶다.");
+data_list.push("나는 지금 같이 있는 사람을 속인 적이 있다.");
+data_list.push("나는 거짓말을 잘한다.");
+data_list.push("나는 지금 너무 재미있다.");
+data_list.push("나는 내 친구들 중 제일 똑똑하다.");
+data_list.push("나는 내 친구들 중 제일 잘생겼다.");
+data_list.push("나는 부지런한 편이다.");
+data_list.push("나는 귀찮으면 연락을 안본다.");
+data_list.push("나는 약속 장소를 갈 때, 현재 위치를 속인 적이 있다.");
+data_list.push("친구들을 만날 때, 안씻고 간적이 있다.");
+data_list.push("나는 멋쟁이다.");
+data_list.push("나는 좀 귀엽다.");
